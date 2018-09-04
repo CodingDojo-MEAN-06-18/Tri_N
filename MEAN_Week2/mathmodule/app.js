67093c1869f8e439fa41be22a88a3d8d481acf35
Tri Nguyen <trisarah2b@gmail.com>
2018-09-03 22:34:05 -0400

    update assignments

---
+++ MEAN_Week2/mathmodule/app.js
@@ -0,0 +1,13 @@
+const MathLibrary = require("./mathlib");
+const math = new MathLibrary(); 
+
+const a =1;
+const b = 35;
+
+console.log(math.add(a,b));
+console.log(math.multiply(a,b));
+console.log(math.square(a));
+console.log(math.random(a, b));
+console.log(math.random(a, b));
+console.log(math.random(a, b));
+console.log(math.random(a, b));
