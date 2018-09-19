fe020efc2a5e7a465090ef37181087152e50eee0
Tri Nguyen <trisarah2b@gmail.com>
2018-09-18 21:01:19 -0400

    httpservice add

---
+++ bookAPI/server/routes/routes.js
@@ -0,0 +1,27 @@
+const bodyParser = require('body-parser'),
+  Author = require('../models/book.js');
+
+module.exports = function (app) {
+  app.use(bodyParser.json());
+
+  app.get('/authors', function (req, res) {
+    Author.author.find({}, function (err, data) {
+      if (err) {
+        res.json({ message: "Error", error: err });
+      } else {
+        res.json({ data });
+      }
+    });
+  });
+  app.post('/new', function (req, res) {
+    console.log(req.body);
+    const newAuthor = new Author.author({ first_name: req.body.firstName, last_name: req.body.lastName, country: req.body.country, birthday: req.body.birthday });
+    newAuthor.save(function (err, data) {
+      if (err) {
+        res.json({ message: "Error", error: err });
+      } else {
+        res.json({ message: "Success: ", data });
+      }
+    });
+  })
+};
